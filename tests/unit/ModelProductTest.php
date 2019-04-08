b47c24a76221940d2553abdc3805e832319fd251
Nicholas Rolando <nicklamort@gmail.com>
2019-04-08 15:58:03 -0600

    adding phpunit

---
+++ tests/unit/ModelProductTest.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Code by: Nick Rolando
+ */
+
+use PHPUnit\Framework\TestCase;
+
+class ModelProductTest extends TestCase
+{
+    public function testSetAndGetName() {
+        $this->assertTrue(true);
+    }
+}
