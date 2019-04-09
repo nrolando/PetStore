b526c39d747f0d4b137dd2342718a887ff443825
Nicholas Rolando <nicklamort@gmail.com>
2019-04-09 11:20:55 -0600

    add CollectionTest.php

---
+++ tests/unit/CollectionTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Code by: Nick Rolando
+ */
+
+use PHPUnit\Framework\TestCase;
+
+class CollectionTest extends TestCase
+{
+    /** @test */
+    public function instantiatedCollectionReturnsNoItems() {
+        
+    }
+}
